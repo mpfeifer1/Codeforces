8fa7c916d11d4662c0130296154726188019696a
Michael Pfeifer <mikepf97@gmail.com>
2017-12-11 14:30:10 -0700

    Solved Two Paths

---
+++ 014d.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dfs(vector<vector<int>>& adj, vector<bool>& vis, int curr, int ban1, int ban2) {
+    if(vis[curr]) {
+        return 0;
+    }
+
+    int best = 0;
+    vis[curr] = true;
+    for(auto i : adj[curr]) {
+        if(curr == ban1 && i == ban2) {
+            continue;
+        }
+        if(curr == ban2 && i == ban1) {
+            continue;
+        }
+        best = max(best, dfs(adj, vis, i, ban1, ban2));
+    }
+
+    return best + 1;
+}
+
+int getlen(vector<vector<int>>& adj, int ban1, int ban2) {
+    // See what can be reached on either side
+    int n = adj.size();
+    vector<bool> vis(n, false);
+    dfs(adj, vis, ban1, ban1, ban2);
+
+    // Split into sides
+    vector<int> l;
+    vector<int> r;
+    for(int i = 0; i < vis.size(); i++) {
+        if(vis[i]) {
+            l.push_back(i);
+        }
+        else {
+            r.push_back(i);
+        }
+    }
+
+    // Find best on left
+    int bestl = 0;
+    for(auto i : l) {
+        vis.clear();
+        vis.resize(n, false);
+        bestl = max(bestl, dfs(adj, vis, i, ban1, ban2));
+    }
+
+    // Find best on right
+    int bestr = 0;
+    for(auto i : r) {
+        vis.clear();
+        vis.resize(n, false);
+        bestr = max(bestr, dfs(adj, vis, i, ban1, ban2));
+    }
+
+    bestl--;
+    bestr--;
+
+    // Return answer
+    return bestl * bestr;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> adj(n);
+    vector<pair<int,int>> edges;
+
+    for(int i = 0; i < n-1; i++) {
+        int n1, n2;
+        cin >> n1 >> n2;
+        n1--;
+        n2--;
+        adj[n1].push_back(n2);
+        adj[n2].push_back(n1);
+        edges.push_back({n1,n2});
+    }
+
+    int best = 0;
+    for(auto i : edges) {
+        best = max(best, getlen(adj, i.first, i.second));
+    }
+
+    cout << best << endl;
+}
