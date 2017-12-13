56d8a6bd67e0e987d68527c5b9deb594d64a525a
Michael Pfeifer <mikepf97@gmail.com>
2017-12-12 21:41:40 -0700

    Solved George and Accomodation

---
+++ 467a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int total = 0;
+    for(int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        if(a + 2 <= b) {
+            total++;
+        }
+    }
+
+    cout << total << endl;
+}
