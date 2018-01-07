35922ed625d07da235b53ced2c1c48d379197787
Michael Pfeifer <mikepf97@gmail.com>
2018-01-07 00:57:15 -0600

    Started Destroying Array

---
+++ 722c.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int find(ll* d, int a) {
+    if(d[a] < 0) {
+        return a;
+    }
+
+    d[a] = find(d, d[a]);
+    return d[a];
+}
+
+void join(ll* d, int a, int b) {
+    a = find(d, a);
+    b = find(d, b);
+
+    if(a == b) {
+        return;
+    }
+
+    ll val = d[a] + d[b];
+    d[a] = val;
+    d[b] = a;
+}
+
+ll val(ll* d, int a) {
+    a = find(d, a);
+    return -d[a];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    ll d[100001];
+    for(int i = 0; i < n; i++) {
+        cin >> d[i];
+        d[i] = -d[i];
+    }
+
+    stack<int> q;
+    stack<int> a;
+
+    for(int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        q.push(temp-1);
+    }
+
+    bool seen[100001];
+    for(int i = 0; i < n; i++) {
+        seen[i] = false;
+    }
+
+    ll best = 0;
+    while(!q.empty()) {
+        int curr = q.top();
+        q.pop();
+
+        a.push(best);
+        seen[curr] = true;
+
+        if(curr > 0 && seen[curr-1]) {
+            join(d, curr-1, curr);
+        }
+        if(curr+1 < n && seen[curr+1]) {
+            join(d, curr+1, curr);
+        }
+
+        for(int i = 0; i < n; i++) {
+            cout << d[i] << " ";
+        }
+        cout << endl;
+
+        best = max(best, val(d, curr));
+    }
+
+    while(!a.empty()) {
+        cout << a.top() << endl;
+        a.pop();
+    }
+}
