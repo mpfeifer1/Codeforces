31ccd3c6ea3ef1a73181b84033b5379ff0d6b702
Michael Pfeifer <mikepf97@gmail.com>
2018-01-12 13:47:57 -0700

    Started first version of template

---
+++ template.cpp
@@ -0,0 +1,41 @@
+// Michael Pfeifer
+// Version 01, 2018/01/12
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 2147483647;
+const ll  inf  = 9223372036854775807ll;
+const int mod  = 1000000007;
+
+#define endl '\n'
+#define pb push_back
+#define D(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+
+
+int main() {
+
+}
