beb8d01142b7642e022540aca932194fe7006d51
Michael Pfeifer <mikepf97@gmail.com>
2018-02-22 20:36:59 -0700

    Competed in a contest

---
+++ 939d.cpp
@@ -0,0 +1,104 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+int find(vector<int>& d, int a) {
+    if(d[a] == -1) {
+        return a;
+    }
+    d[a] = find(d, d[a]);
+    return d[a];
+}
+
+void join(vector<int>& d, int a, int b) {
+    a = find(d, a);
+    b = find(d, b);
+
+    if(a == b) {
+        return;
+    }
+
+    d[a] = b;
+}
+
+int main() {
+    //file();
+    //fast();
+
+    int n;
+    cin >> n;
+
+    vector<int> d(26, -1);
+
+    vector<pair<char,char>> ans;
+
+    string s1, s2;
+    cin >> s1 >> s2;
+    for(int i = 0; i < n; i++) {
+        int n1 = s1[i]-'a';
+        int n2 = s2[i]-'a';
+
+        if(find(d, n1) == find(d, n2)) {
+            continue;
+        }
+
+        join(d, n1, n2);
+        ans.pb({s1[i],s2[i]});
+    }
+
+    cout << ans.size() << endl;
+    for(auto i : ans) {
+        cout << i.first << " " << i.second << endl;
+    }
+
+    return 0;
+}
