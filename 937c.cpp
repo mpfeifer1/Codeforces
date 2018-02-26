160b72daeee12486bec63d990dbaf28919c26098
Michael Pfeifer <mikepf97@gmail.com>
2018-02-26 10:33:08 -0700

    Solved Save Energy

---
+++ 937c.cpp
@@ -0,0 +1,86 @@
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
+
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+void file() {
+    auto a = freopen("a.in",  "r", stdin);
+    auto b = freopen("a.out", "w", stdout);
+    if(!a || !b) cout << "uh oh" << endl;
+}
+
+
+
+
+
+int main() {
+    //file();
+    //fast();
+
+    cout.precision(12);
+    cout << fixed;
+
+
+
+    ld k, d, t;
+    cin >> k >> d >> t;
+
+    ld cyclelen = ceil(k/d) * d;
+    ld on = k;
+    ld off = cyclelen-k;
+
+    ld cooked = on + off/2.0;
+    ld cycles = floor(t/cooked);
+    ld actualcooked = cycles*cooked;
+
+    ld timesofar = cycles * cyclelen;
+
+    t -= actualcooked;
+
+    if(t > on) {
+        cout << timesofar + on + (t-on)*2 << endl;
+    }
+    else {
+        cout << timesofar + t << endl;
+    }
+
+    return 0;
+}
